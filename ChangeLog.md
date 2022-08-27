059a31b37b9ca0cc83ac7f3258b1c883a9fa737c
Carlos Rueda <carueda@mbari.org>
2022-08-26 19:04:35 -0700

    disable subscription stuff for now

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,5 +1,6 @@
 2022-08
 
+- disable subscription stuff for now
 - adjustment to enable ui dispatch from mxm-server (via quarkus-quinoa)
   - some tests with history mode
 - some basic setup for subscriptions, but seems limited/nonfunctional until migrating to vue3
--- src/boot/apollo.js
+++ src/boot/apollo.js
@@ -4,9 +4,11 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
 import { loadConfig } from 'boot/mxmConfig'
 
+/*
 import { split } from 'apollo-link'
 import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
+*/
 
 let apolloClient = null;
 
@@ -41,24 +43,27 @@ export default async ({ store, app, Vue }) => {
         uri: graphqlUri,
       })
 
-      const wsUri = graphqlUri.replace(/^http/, 'ws')
-      const wsLink = new WebSocketLink({
-        uri: wsUri,
-        options: {
-          reconnect: true,
-        },
-      })
+      const link = httpLink
+      /*
+            const wsUri = graphqlUri.replace(/^http/, 'ws')
+            const wsLink = new WebSocketLink({
+              uri: wsUri,
+              options: {
+                reconnect: true,
+              },
+            })
 
-      const link = split(
-        // split based on operation type
-        ({ query }) => {
-          const definition = getMainDefinition(query)
-          return definition.kind === 'OperationDefinition' &&
-            definition.operation === 'subscription'
-        },
-        wsLink,
-        httpLink
-      )
+            const link = split(
+              // split based on operation type
+              ({ query }) => {
+                const definition = getMainDefinition(query)
+                return definition.kind === 'OperationDefinition' &&
+                  definition.operation === 'subscription'
+              },
+              wsLink,
+              httpLink
+            )
+      */
 
       const cache = new InMemoryCache({
         // NOTE: the following not available in the used vue2 setup, but available in vue3.
